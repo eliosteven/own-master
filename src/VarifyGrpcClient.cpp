c257cd1cec832b0ac105b36d86febc540777c2cd
eliosteven <398172808@qq.com>
2025-08-12 23:52:25 +0800

    Move gateserver-related files into gateserver directory

--- src/VarifyGrpcClient.cpp
+++
@@ -1,9 +0,0 @@
-#include "VarifyGrpcClient.h"
-#include "ConfigMgr.h"
-
-VerifyGrpcClient::VerifyGrpcClient() {
-    auto& gCfgMgr = ConfigMgr::Inst();
-    std::string host = gCfgMgr["VarifyServer"]["Host"];
-    std::string port = gCfgMgr["VarifyServer"]["Port"];
-    pool_.reset(new RPConPool(5, host, port));
-}
